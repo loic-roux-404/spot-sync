69683f27ff65448724ef74f0b2d984bc8f0b0032
loic-roux <69loloro10@gmail.com>
2021-03-02 12:45:08 +0100

    every time delete / cron list command

--- setup-cron.sh
+++ setup-cron.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PLAYLISTS_FILE=${PLAYLISTS_FILE:-playlists.txt}
-DELETE=${DELETE:-0}
+DELETE=${DELETE:-1}
+LIST=${LIST:-0}
 
 setup() {
-    local min=${1:-0}
-    local hour=${2:-3}
+    local min=${1:-*}
+    local hour=${2:-*}
     local day=${3:-4}
 
     (crontab -l 2>/dev/null; \
@@ -17,4 +18,10 @@ delete() {
     crontab -l | grep -v './new-dl.sh' | crontab - || return 0
 }
 
-delete && setup $@
+list() {
+    [ $LIST -eq 1 ] && crontab -l
+}
+
+list
+delete && \
+setup $@
