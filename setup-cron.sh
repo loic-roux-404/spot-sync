3b3df345bfa24cf3054c75fb22e7e073cbedf8a9
loic-roux <69loloro10@gmail.com>
2021-04-25 20:17:20 +0200

    new playlist

--- setup-cron.sh
+++ setup-cron.sh
@@ -10,12 +10,12 @@ setup() {
     local day=${3:-4}
 
     (crontab -l 2>/dev/null; \
-    echo "${min} ${hour} ${day} * * ./new-dl.sh > ${PLAYLISTS_FILE}.log") | crontab -
+    echo "${min} ${hour} ${day} * * $(pwd)/new-dl.sh > ${PLAYLISTS_FILE}.log") | crontab -
 }
 
 delete() {
     [ $DELETE -eq 1 ] && \
-    crontab -l | grep -v './new-dl.sh' | crontab - || return 0
+    crontab -l | grep -v "$(pwd)/new-dl.sh" | head -1 | crontab - || return 0
 }
 
 list() {
